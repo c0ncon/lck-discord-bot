0e941562991274d1da4c50a233ed250bee166b02
c0ncon <c4rneade@gmail.com>
2019-01-23 16:52:26 +0900

    get bot token from file & image response

--- src/lck-bot/lck-bot.go
+++ src/lck-bot/lck-bot.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/signal"
+	"regexp"
 	"strings"
 	"syscall"
 	"time"
@@ -14,9 +17,15 @@ import (
 )
 
 var (
-	Token      string
-	schedules  Schedules
-	WeekdayKor = [...]string{"일", "월", "화", "수", "목", "금", "토"}
+	token            string
+	schedules        Schedules
+	weekdayKor       = [...]string{"일", "월", "화", "수", "목", "금", "토"}
+	imgRespRegexp, _ = regexp.Compile("^\\(([\\w\\d\\s가-힣]+)\\)$")
+	imageUrls        = map[string]string{
+		"씨밸럼아": "https://pbs.twimg.com/media/Dm3RgfxUUAAanib.jpg:orig",
+		"띠용":   "https://pbs.twimg.com/media/Ds6EGTCU0AED2XJ.jpg:orig",
+		"상남자":  "https://pbs.twimg.com/media/Dr3F_c1U0AA60Zw.jpg:orig",
+	}
 )
 
 type Schedules struct {
@@ -31,12 +40,12 @@ type Schedule struct {
 type Match []string
 
 func init() {
+	loadToken(&token)
 	loadSchedules("schedules.json", &schedules)
 }
 
 func main() {
-	Token = ""
-	dg, err := discordgo.New("Bot " + Token)
+	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
@@ -50,7 +59,14 @@ func main() {
 		return
 	}
 
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	user, err := dg.User("@me")
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	fmt.Println("Bot invite URL:")
+	fmt.Printf("\thttps://discordapp.com/oauth2/authorize?client_id=%s&scope=bot\n", user.ID)
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
@@ -63,14 +79,60 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if m.Content == "뜬뜬" || m.Content == "ㄸㄸ" {
-		t, _ := time.Parse(time.RFC3339, string(m.Timestamp))
-		t = t.Add(9 * time.Hour)
+	matched, t := stringMatch(m.Content)
+	if matched {
 		c, _ := s.State.Channel(m.ChannelID)
 		g, _ := s.State.Guild(c.GuildID)
-		fmt.Printf("%s %s@%s#%s\n", t.Format("2006-01-02 15:04"), m.Author.Username, g.Name, c.Name)
-		s.ChannelMessageSend(m.ChannelID, getNextMatch())
+		msg := fmt.Sprintf("%s: %s@%s#%s", t, m.Author.Username, g.Name, c.Name)
+		consoleLog(m.Timestamp, msg)
+
+		switch t {
+		case "s":
+			s.ChannelMessageSend(m.ChannelID, getNextMatch())
+			return
+		case "i":
+			str := imgRespRegexp.FindStringSubmatch(m.Content)[1]
+			if imageUrls[str] != "" {
+				s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+					Image: &discordgo.MessageEmbedImage{
+						URL: imageUrls[str],
+					},
+				})
+			}
+			return
+		}
 	}
+}
+
+func stringMatch(str string) (bool, string) {
+	if str == "뜬뜬" || str == "ㄸㄸ" {
+		return true, "s"
+	} else if imgRespRegexp.MatchString(str) {
+		return true, "i"
+	} else {
+		return false, ""
+	}
+}
+
+func consoleLog(timestamp discordgo.Timestamp, msg string) {
+	t, _ := time.Parse(time.RFC3339, string(timestamp))
+	t = t.Add(9 * time.Hour)
+	fmt.Printf("%s %s\n", t.Format("2006-01-02 15:04"), msg)
+}
+
+func loadToken(token *string) {
+	var t string
+	if _, err := os.Stat(".token"); err == nil {
+		tokenFile, _ := os.Open(".token")
+		defer tokenFile.Close()
+		scanner := bufio.NewScanner(tokenFile)
+		scanner.Scan()
+		t = scanner.Text()
+	} else {
+		flag.StringVar(&t, "t", "", "Bot Token")
+		flag.Parse()
+	}
+	*token = t
 }
 
 func loadSchedules(path string, sc *Schedules) {
@@ -81,7 +143,10 @@ func loadSchedules(path string, sc *Schedules) {
 	}
 	defer jsonFile.Close()
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		panic(err)
+	}
 	json.Unmarshal(byteValue, sc)
 }
 
@@ -99,7 +164,7 @@ func getNextMatch() string {
 				matches[i] = strings.Join(m, " vs ")
 			}
 
-			nextMatch = sc.Date + "(" + WeekdayKor[t.Weekday()] + "): " + strings.Join(matches, " / ")
+			nextMatch = sc.Date + "(" + weekdayKor[t.Weekday()] + "): " + strings.Join(matches, " / ")
 			break
 		}
 	}
