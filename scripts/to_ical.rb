19ffd94facaf7e9624e5a362b6fb0d850607068f
c0ncon <c4rneade@gmail.com>
2020-09-08 00:01:08 +0900

    bugfix ical script

--- scripts/to_ical.rb
+++ scripts/to_ical.rb
@@ -12,13 +12,20 @@ puts 'VERSION:2.0'
 puts 'PRODID:-//ME//LCK//EN'
 puts 'X-WR-CALNAME:LCK'
 puts 'X-WR-TIMEZONE:Asia/Seoul'
+
 data.each do |match|
   year, month, day = match['date'].split('-').map(&:to_i)
   hour, minute = match['time'].split(':').map(&:to_i)
-  dt = DateTime.new(year, month, day, hour, minute, 0, '+09:00')
+  if hour >= 24
+    h = hour % 24
+    dt = DateTime.new(year, month, day, h, minute, 0, '+09:00')
+    dt = dt.next_day(1)
+  else
+    dt = DateTime.new(year, month, day, hour, minute, 0, '+09:00')
+  end
 
   puts 'BEGIN:VEVENT'
-  puts "DTSTART: #{dt.new_offset(0).strftime("%Y%m%dT%H%M%SZ")}"
+  puts "DTSTART: #{dt.new_offset(0).strftime('%Y%m%dT%H%M%SZ')}"
   puts "SUMMARY:#{match['home']} vs #{match['away']}"
   puts 'END:VEVENT'
 end
