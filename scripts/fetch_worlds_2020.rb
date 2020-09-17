ec4f6af14d3835667a1bf63caf32e89d1d6fb210
c0ncon <c4rneade@gmail.com>
2020-09-17 11:25:49 +0900

    Worlds 2020 Play-In Group Stage

---
+++ scripts/fetch_worlds_2020.rb
@@ -0,0 +1,31 @@
+require 'faraday'
+require 'json'
+require 'date'
+
+url = 'https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=ja-JP&leagueId=98767975604431411'
+header = { 'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z' }
+
+events = []
+newer = true
+until newer.nil?
+  response = Faraday.get url do |req|
+    req.headers = header
+  end
+  res = JSON.parse response.body
+  events += res['data']['schedule']['events']
+
+  newer = res['data']['schedule']['pages']['newer']
+  url += "&pageToken=#{newer}"
+end
+events.select! { |e| e['state'] == 'unstarted' }
+
+latest_schedule = events.map do |event|
+  jst = DateTime.parse(event['startTime']).to_time + (3600 * 9)
+  {
+    date: jst.strftime('%F'),
+    time: jst.strftime('%R'),
+    home: event['match']['teams'].first['code'],
+    away: event['match']['teams'].last['code']
+  }
+end
+puts latest_schedule.to_json
